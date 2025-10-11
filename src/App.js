927246373ef012c2ec056a7587e15c0058e44a85
sshrey15 <stud22.sks2@gec.ac.in>
2025-10-11 16:47:48 +0530

    breadcrumb

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
-import Breadcrumb from "./components/Layout/Breadcrumb";
+// import Breadcrumb from "./components/Layout/Breadcrumb";
 import ChatbotButton from "./components/Chatbot";
 import ScrollToTop from "./components/ScrollToTop";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
@@ -73,8 +73,7 @@ function App() {
       {/* 🌐 Header */}
       <Header />
       
-      {/* 📍 Breadcrumb Navigation */}
-      <Breadcrumb />
+  // Breadcrumb removed
       
       <ScrollToTop />
       {/* 📄 Main Page Content */}
